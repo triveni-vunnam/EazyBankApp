3adc123b5dfcd9f34900257d3119fe0e26208779
Thiru-vunnam <128240044+Thiru-vunnam@users.noreply.github.com>
2023-03-27 19:40:40 +0530

    change1

--- EazyBankApplication/src/main/java/com/eazybank/controller/AccountController.java
+++ EazyBankApplication/src/main/java/com/eazybank/controller/AccountController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
+	//this is account controller get mapping
 	@GetMapping("/myAccount")
 	public String getAccountDetails() {
 		return "Here are the account details from the DB you can check it out";
 	}
+	//end
 }
