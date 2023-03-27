252ee2bd279ac837390869e16fc48f7cb2fcd52a
Thiru-vunnam <128240044+Thiru-vunnam@users.noreply.github.com>
2023-03-27 19:43:07 +0530

    change2

--- EazyBankApplication/src/main/java/com/eazybank/controller/BalanceController.java
+++ EazyBankApplication/src/main/java/com/eazybank/controller/BalanceController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BalanceController {
-
+//this is balance controller
     @GetMapping("/myBalance")
     public String getBalanceDetails() {
         return "Here are the balance details from the DB you can check it out";
